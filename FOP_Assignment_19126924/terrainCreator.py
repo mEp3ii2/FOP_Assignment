38ddac5b8088f823a940d93c03a540976bbf820b
mEp3ii2 <74845700+mEp3ii2@users.noreply.github.com>
2022-09-27 09:33:14 +0800

    Add files via upload

---
+++ FOP_Assignment_19126924/terrainCreator.py
@@ -0,0 +1,20 @@
+import numpy as np
+import random
+
+
+map = np.zeros((10000,2))
+
+#creature = 1
+#rock = 2
+# water = 3
+
+
+
+print(np.size(map))
+print(np.shape(map))
+
+print(map)
+print(len(map))
+
+map[1][1]
+
